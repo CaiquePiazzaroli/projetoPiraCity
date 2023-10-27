135cd984bfbcc0dd18d77532600b85304536ad74
caique <caique405@gmail.com>
2023-10-27 09:13:12 -0300

    Ajustes Visuais

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let planoDeFundo = document.querySelector(".cabecalho");
 let listaImagens = ["1.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg"];
+let linkMenuHamburguer = document.querySelectorAll(".lista__hamburguer__link");
+let caixaCheckBox = document.querySelector("#caixa__checkbox");
+
+
+
 let numAleatorioAnt = 0;
 let numAleatorio = Math.floor(Math.random() * listaImagens.length);
 const timer = setInterval(() => {
@@ -9,3 +14,10 @@ const timer = setInterval(() => {
     }
     numAleatorioAnt = numAleatorio;
 },"5000");
+
+
+for(var i = 0; i < linkMenuHamburguer.length; i++){
+    linkMenuHamburguer[i].addEventListener("click", () => {
+        caixaCheckBox.checked = false;
+    })
+}
